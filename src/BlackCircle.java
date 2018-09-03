0511773f26966be876b4d8a5d72d397a4d6453b4
Luiz H. Cardoso <c.luizhenrique@gmail.com>
2018-09-03 08:34:38 -0300

    Submodule test 2

--- src/BlackCircle.java
+++ src/BlackCircle.java
@@ -3,5 +3,5 @@ public class BlackCircle implements DrawAPI{
     @Override
     public void drawCircle(int radius, int x, int y) {
         System.out.println("Drawing Circle [color: black, radius: "+ radius+", x: "+x+"y: "+y+" ]");
-    }
+    }//teste
 }
